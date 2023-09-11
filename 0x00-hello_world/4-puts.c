c914cafa77b369886b75ecc1489d76742f5a3736
AbidemiDev <obamisaiah8@gmail.com>
2023-09-11 03:35:07 -0700

    print with \puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main: Print Programming is like building a multilingual puzzle
+ * main -  Print Programming is like building a multilingual puzzle
  * return: Return 0 on success
  */
 int main(void)
