f63a39f90a65bf5e18d48f5a30ad3e37eadb1265
AbidemiDev <obamisaiah8@gmail.com>
2023-09-18 04:38:49 -0700

    function that check for positove or negative integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -  test for a postitive_or_negative
+ * Return: Retuen 0 on sucess
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+
+	return (0);
+}
