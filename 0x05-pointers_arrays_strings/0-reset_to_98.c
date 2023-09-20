d451ea2c2b1f7cde8150847e62a472ad9ac77c38
AbidemiDev <obamisaiah8@gmail.com>
2023-09-19 20:23:46 -0700

    Main header files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function that takes a pointer to an int as parameter
+ * Return: 0 on sucess
+ */
+void reset_to_98(int *n)
+{
+	int np;
+	*n = np;
+
+	np = 98;
+	return (0);
+}
